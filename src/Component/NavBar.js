b1eda860646b864ace64909e84d8ce4256da7470
Aviatrix_ui <varshamodake@gmail.com>
2020-10-06 14:51:43 +0100

    responsive

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,18 +1,43 @@
-import React,{Component} from 'react';
+import React,{useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import '../CSS/NavBar.css';
-
+import {faAlignJustify} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar=({children})=>{
+
+  useEffect(()=>{
+    var LinkArray=document.querySelectorAll("li");
+    LinkArray.forEach(
+      (e)=>{
+      e.addEventListener('click',()=>{
+        var currentActive=document.getElementsByClassName("Active");
+        if(currentActive.length >0)
+        {
+          currentActive[0].className = currentActive[0].className.replace("Active", "");
+         
+        }
+        
+          e.classList.add("Active");
+         
+        
+      })
+      }
+  )
+    }
+  )
   return (
     <div className="mainWrapper">
+      
         <div className="navBarWrapper">
-          
+        
+        
             <ul id="navbar">
-           <li> <Link to='/'>About Me</Link></li>
-           <li> <Link to='/Jokes'>Jokes</Link></li>
-           <li> <Link to="/Contact">Contact Me</Link></li>
+           <li className="tab"> <Link to='/' >About Me</Link></li>
+           <li className="tab"> <Link to='/Jokes'>Jokes</Link></li>
+           <li className="tab"> <Link to='/Contact' >Contact Me</Link></li>
             </ul> 
+           
             </div>
             {children}
             </div>
